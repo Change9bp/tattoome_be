f9e9762cb10bfb2755f74295e4df74de76c58942
Alessandro <ale.contestabile@gmail.com>
2023-11-13 00:12:17 +0100

    secondo commit

---
+++ middlewares/validatorUserCreator.js
@@ -0,0 +1,73 @@
+const validator = require("validator");
+
+const validatorUserCreator = (req, res, next) => {
+  const errors = [];
+
+  const {
+    name,
+    lastName,
+    email,
+    password,
+    alias,
+    avatar,
+    tattooStyle,
+    nation,
+    region,
+    city,
+    address,
+  } = req.body;
+
+  //funzione di verifica url
+  const isImageURL = (avatar) => {
+    const allowedExtensions = [".jpg", ".jpeg", ".png", ".gif"];
+    if (!validator.isURL(avatar)) {
+      return false;
+    }
+    const fileExtension = avatar.substring(avatar.lastIndexOf("."));
+    return allowedExtensions.includes(fileExtension.toLowerCase());
+  };
+
+  if (name && !validator.isString(name)) {
+    errors.push("Name must be a non-empty string");
+  }
+  if (lastName && !validator.isString(lastName)) {
+    errors.push("Last name must be a non-empty string");
+  }
+  if (password && !validator.isString(password)) {
+    errors.push("Password must be a non-empty string");
+  }
+  if (email && (!validator.isString(email) || !validator.isEmail(email))) {
+    errors.push("Email must be a valid email address");
+  }
+
+  if (alias && !validator.isString(alias)) {
+    errors.push("alias must be a non-empty string");
+  }
+
+  if (avatar && !isImageURL(avatar)) {
+    errors.push("Avatar must contain a correct image format");
+  }
+
+  if (tattooStyle.length > 0 && !Array.isArray(avatar)) {
+    errors.push("tattooStyle must be an array with data");
+  }
+  if (errors.length > 0) {
+    return res.status(400).json({ errors });
+  }
+  if (nation && !validator.isString(nation)) {
+    errors.push("nation must be a non-empty string");
+  }
+  if (region && !validator.isString(region)) {
+    errors.push("region must be a non-empty string");
+  }
+  if (city && !validator.isString(city)) {
+    errors.push("city must be a non-empty string");
+  }
+  if (address && !validator.isString(address)) {
+    errors.push("address must be a non-empty string");
+  }
+
+  next();
+};
+
+module.exports = validatorAuthor;
--- routes/tattooPosts.js
+++ routes/tattooPosts.js
@@ -41,3 +41,164 @@ tattooPost.post(
   }
 );
 /* #endregion */
+
+//GET con pagination
+tattooPost.get(
+  "/tattooPost",
+  /*verifyToken,*/ async (req, res) => {
+    const { page, pageSize } = req.query;
+    console.log("page", page, "pagesize", pageSize);
+    const posts = await PostModel.find()
+      .populate({
+        path: "userCreator",
+        select: "_id name lastName email avatar",
+      })
+      .limit(pageSize)
+      .skip((page - 1) * pageSize);
+
+    const totalPosts = await PostModel.count();
+
+    try {
+      res.status(200).send({
+        statusCode: 200,
+        currentPage: Number(page),
+        totalPages: Math.ceil(totalPosts / pageSize),
+        posts,
+      });
+    } catch (error) {
+      res.status(500).send({
+        statusCode: 500,
+        message: "internal server error",
+      });
+    }
+  }
+);
+
+//GET BY ID
+
+tattooPost.get("/tattooPost/:_id", async (req, res) => {
+  const { _id } = req.params;
+
+  const post = await PostModel.findById(_id);
+  if (!post) {
+    return res.status(404).send({
+      statusCode: 404,
+      message: "ID post dosent exists",
+    });
+  }
+
+  try {
+    res.status(200).send({
+      statusCode: 200,
+      message: "post finded",
+      post,
+    });
+  } catch (error) {
+    res.status(500).send({
+      statusCode: 500,
+      message: "Server internal error",
+    });
+  }
+});
+
+//POST
+tattooPost.post(
+  "/tattooPost",
+  /*verifyToken, validatorPost,*/ async (req, res) => {
+    const addPost = new PostModel({
+      tattooStyle: req.body.tattooStyle,
+      title: req.body.title,
+      cover: req.body.cover,
+      author: req.body.author,
+      content: req.body.content,
+    });
+
+    try {
+      const post = await addPost.save();
+
+      res.status(200).send({
+        statusCode: 200,
+        message: "post saved successfully",
+        payload: post,
+      });
+    } catch (error) {
+      res.status(500).send({
+        statusCode: 500,
+        message: "Server internal error",
+        error,
+      });
+    }
+  }
+);
+
+//PATCH
+
+tattooPost.patch(
+  "/tattooPost/:_id",
+  /*verifyToken, validatorPost,*/ async (req, res) => {
+    const { _id } = req.params;
+    const postExists = await PostModel.findById(_id);
+    if (!postExists) {
+      return res.status(404).send({
+        statusCode: 404,
+        message: "Post dosent exsists or already deleted",
+      });
+    }
+
+    try {
+      const dataToUpdate = req.body;
+      const options = { new: true };
+      const result = await PostModel.findByIdAndUpdate(
+        _id,
+        dataToUpdate,
+        options
+      );
+
+      res.status(200).send({
+        statusCode: 200,
+        message: "Post edited correctly",
+        result,
+      });
+    } catch (error) {
+      res.status(500).send({
+        statusCode: 500,
+        message: "Server internal error",
+        error,
+      });
+    }
+  }
+);
+
+//DELETE
+
+tattooPost.delete(
+  "/tattooPost/:_id",
+  /*verifyToken, validatorPost,*/ async (req, res) => {
+    const { _id } = req.params;
+
+    const delPost = await PostModel.findById(_id);
+    if (!delPost) {
+      return res.status(404).send({
+        statusCode: 404,
+        message: "Post dosent exsists or already deleted",
+      });
+    }
+    try {
+      const postToDelete = await PostModel.findByIdAndDelete(_id);
+
+      res.status(200).send({
+        statusCode: 200,
+        message: "post correctly deleted",
+        postToDelete,
+      });
+    } catch (error) {
+      res.status(500).send({
+        statusCode: 500,
+        message: "Server internal error",
+        error,
+      });
+    }
+  }
+);
+
+module.exports = tattooPost;
